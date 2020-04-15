0989e6488d9b54326f832b4f86024ffe251d5357
lashinijay <lashinisharikaj@gmail.com>
2020-04-15 16:17:50 +0530

    changed the type of variable to val

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,17 +1,15 @@
-import java.util
-
 object Parser {
 
-  def parser(method: String , uri :String, body :String):String={
+  def parser(method: String , uri :String, body :String): String = {
     var response = ""
     if ("GET" == method){
       val name = uri.split("=")(1)
       if (uri.split("=")(0) == "isbn") Services.search(name)
       else Services.viewList(name)
     }
-    else if ("POST"==method)
+    else if ("POST" == method)
         Services.add(body)
-    else{"Null"}
+    else "Enter a valid URL"
   }
 
 }
