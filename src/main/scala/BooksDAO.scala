06a79bd684a887cb6c328b1c81ac63000c35e6e5
lashinijay <lashinisharikaj@gmail.com>
2020-04-15 15:40:26 +0530

    changed the type of variable to val

--- src/main/scala/BooksDAO.scala
+++ src/main/scala/BooksDAO.scala
@@ -3,18 +3,18 @@ import collection.JavaConverters._
 
 object BooksDAO {
   def addbook(book: Book): String = {
-    var ls = BookList.ls
-    (ls += book)
+    val ls = BookList.ls
+    ls += book
     "book was successfully added"
   }
 
   def bookdetails(t: String): Book = {
-    var ls = BookList.ls
-    (ls.filter(Book=> Book.isbn ==t)(0))
+    val ls = BookList.ls
+    ls.filter(Book=> Book.isbn ==t)(0)
   }
 
   def viewlist(t: String): util.List[Book] ={
-    var ls = BookList.ls
-    ls.filter( Book => (Book.author == t || Book.title == t)).asJava
+    val ls = BookList.ls
+    ls.filter( Book => Book.author == t || Book.title == t).asJava
   }
 }
