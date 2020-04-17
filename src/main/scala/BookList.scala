f3bf8242e1fbb130d4a51d9c15250c1edf073c74
lashinijay <lashinisharikaj@gmail.com>
2020-04-17 17:42:48 +0530

    updated variable names

--- src/main/scala/BookList.scala
+++ src/main/scala/BookList.scala
@@ -7,6 +7,6 @@ object BookList {
   val book4 = Book("234-0-340-999-734","Paradise Bay","James G.",3600)
   val book5 = Book("34-9088-5512-8","The Leopard","Jo Nesbo",1500)
   val book6 = Book("190-76-664-12","The Sun","N. Roberts",255.90)
-  var ls = ListBuffer(book1,book2,book3,book4,book5,book6)
+  var list = ListBuffer(book1,book2,book3,book4,book5,book6)
 }
 
--- src/main/scala/Services.scala
+++ src/main/scala/Services.scala
@@ -3,20 +3,20 @@ import scala.jdk.CollectionConverters._
 object Services {
 
   def viewList(term: String): String = {
-    val res = BookList.ls.filter(book => book.author == term || book.title == term).asJava
+    val res = BookList.list.filter(book => book.author == term || book.title == term).asJava
     if (res.isEmpty) "No entry was found"
     else  Parser.toJson(res)
   }
 
   def search(isbn: String): String = {
-    val res = BookList.ls.filter(book => book.isbn == isbn).asJava
+    val res = BookList.list.filter(book => book.isbn == isbn).asJava
     if (res.isEmpty) "No entry was found"
     else  Parser.toJson(res)
   }
 
   def add(sb: String): String = {
     val book = Parser.fromJson(sb)
-    val res = (BookList.ls += book).asJava
+    val res = (BookList.list += book).asJava
     Parser.toJson(res)
   }
 
