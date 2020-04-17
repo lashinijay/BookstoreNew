4308c32f69fb24949c9284b65aa9392f969be45d
lashinijay <lashinisharikaj@gmail.com>
2020-04-17 16:13:30 +0530

    changed the type of variables

--- src/main/scala/BookList.scala
+++ src/main/scala/BookList.scala
@@ -1,12 +1,12 @@
 import scala.collection.mutable.ListBuffer
 
 object BookList {
-  var book1 = Book("190-76-7664-12","The Sun","Andrew",245.90)
-  var book2 = Book("34-9088-55-8","Snowman","Jo Nesbo",1500)
-  var book3 = Book("234-0-340-99912-7","Good Friday","Lynda",3600)
-  var book4 = Book("234-0-340-999-734","Paradise Bay","James G.",3600)
-  var book5 = Book("34-9088-5512-8","The Leopard","Jo Nesbo",1500)
-  var book6 = Book("190-76-664-12","The Sun","N. Roberts",255.90)
+  val book1 = Book("190-76-7664-12","The Sun","Andrew",245.90)
+  val book2 = Book("34-9088-55-8","Snowman","Jo Nesbo",1500)
+  val book3 = Book("234-0-340-99912-7","Good Friday","Lynda",3600)
+  val book4 = Book("234-0-340-999-734","Paradise Bay","James G.",3600)
+  val book5 = Book("34-9088-5512-8","The Leopard","Jo Nesbo",1500)
+  val book6 = Book("190-76-664-12","The Sun","N. Roberts",255.90)
   var ls = ListBuffer(book1,book2,book3,book4,book5,book6)
 }
 
