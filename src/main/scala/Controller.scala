f460df769aa402d8f6b97a829bf0ad9f27c7bf3a
lashinijay <lashinisharikaj@gmail.com>
2020-04-19 11:16:34 +0530

    Added Json handling

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -4,13 +4,23 @@ object Controller {
 
   def controller(bs:ListBuffer[String])={
     val method = bs(0)
-
     if (method.equals("GET")){
       val term = bs(1)
       val name = bs(2)
-      if (term.equals("isbn")) Services.search(name)
-      else Services.viewList(name)}
-    else if (method.equals("POST")) Services.add(bs(1))
+      if (term.equals("isbn")) {
+        val res = Services.search(name)
+        if (res.isEmpty) "No entry was found"
+        else  Parser.toJson(res)}
+      else{
+        val res = Services.viewList(name)
+        if (res.isEmpty) "No entry was found"
+        else  Parser.toJson(res)
+      }}
+    else if (method.equals("POST")) {
+      val book = Parser.fromJson(bs(1))
+      val res = Services.add(book)
+      Parser.toJson(res)
+    }
     else "Invalid Request"
   }
 }
