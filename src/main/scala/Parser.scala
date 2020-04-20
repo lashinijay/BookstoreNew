38ec8581e97f468d83055679babc41c762c7a225
lashinijay <lashinisharikaj@gmail.com>
2020-04-20 17:01:31 +0530

    added parserAddRequest and parserSearchRequest methods, removed HTTP methods checking

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,25 +2,22 @@ import java.util
 
 import com.google.gson.GsonBuilder
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 
 object Parser {
 
-  def parser(method:String,uri:String,body:String)={
-    var buf = ListBuffer(method)
-    if ("GET" == method) {
-      val term = uri.split("=")(0)
-      val name = uri.split("=")(1)
-      buf += term
-      buf += name }
-    else if ("POST"==method){
-      buf += body }
-    else buf }
-
   val gson = new GsonBuilder().setPrettyPrinting().create
 
-  def toJson(res:util.List[Book]): String = {gson.toJson(res)}
+  def toJson(res:util.List[Book]): String = gson.toJson(res)
 
-  def fromJson(sb:String) = {gson.fromJson(sb,classOf[Book])}
+  def fromJson(sb:String) = gson.fromJson(sb,classOf[Book])
 
+  def parseAddRequest(body:String)= fromJson(body)
+
+  def parseSearchRequest(uri:String)={
+    var buf = new mutable.ListBuffer[String]
+    val term = uri.split("=")(0)
+    val name = uri.split("=")(1)
+    buf += term
+    buf += name }
 }
