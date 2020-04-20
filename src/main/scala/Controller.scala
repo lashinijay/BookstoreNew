1abe248260d99a411682502dd18a7e9f2ede7013
lashinijay <lashinisharikaj@gmail.com>
2020-04-20 17:00:26 +0530

    added HTTP method check and opitimized

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -1,27 +1,27 @@
-import scala.collection.mutable.ListBuffer
+import java.util
 
 object Controller {
 
-  def controller(bs:ListBuffer[String])={
-    val method = bs(0)
-    if (method.equals("GET")){
-      val term = bs(1)
-      val name = bs(2)
+  def controller(method:String, uri:String, body:String)={
+    val msg = "Invalid Request"
+    if (method.equals("GET")) {
+      val buf = Parser.parseSearchRequest(uri)
+      val term = buf(0)
+      val value = buf(1)
       if (term.equals("isbn")) {
-        val res = Services.search(name)
-        if (res.isEmpty) "No entry was found"
-        else  Parser.toJson(res)}
+        val res = Services.searchByIsbn(value)
+        checkList(res)}
       else if(term.equals("author")||term.equals("title")){
-        val res = Services.viewList(name)
-        if (res.isEmpty) "No entry was found"
-        else  Parser.toJson(res)}
-      else "Invalid Request"
-    }
-    else if (method.equals("POST")) {
-      val book = Parser.fromJson(bs(1))
-      val res = Services.add(book)
-      Parser.toJson(res)
-    }
-    else "Invalid Request"
-  }
+        val res = Services.searchByName(value)
+        checkList(res)}
+      else msg}
+    else if (method.equals("POST")){
+      val book = Parser.parseAddRequest(body)
+      Services.add(book)
+      "Successfully Added"}
+    else msg }
+
+  def checkList(res:util.List[Book]) = {
+    if (res.isEmpty) "No entry was found"
+    else  Parser.toJson(res)}
 }
