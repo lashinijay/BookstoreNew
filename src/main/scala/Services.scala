82009637e4f13c0bc87d0effc5835f1c986916e7
lashinijay <lashinisharikaj@gmail.com>
2020-04-20 17:02:13 +0530

    modified return types of functions

--- src/main/scala/Services.scala
+++ src/main/scala/Services.scala
@@ -4,10 +4,10 @@ import scala.jdk.CollectionConverters._
 
 object Services {
 
-  def viewList(term: String): util.List[Book] = BookList.list.filter(book => book.author == term || book.title == term).asJava
+  def searchByName(term: String): util.List[Book] = BookList.list.filter(book => book.author == term || book.title == term).asJava
 
-  def search(isbn: String): util.List[Book] = BookList.list.filter(book => book.isbn == isbn).asJava
+  def searchByIsbn(isbn: String): util.List[Book] = BookList.list.filter(book => book.isbn == isbn).asJava
 
-  def add(book: Book): util.List[Book] =  (BookList.list += book).asJava
+  def add(book: Book) = {var bookList = (BookList.list+= book)}
 
 }
